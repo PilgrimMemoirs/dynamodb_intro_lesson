5485fab78fe1ee7711e5ae4e97f9387b82f7e8b4
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-12-14 09:52:54 -0800

    add campsite.rb exercise structure notes

---
+++ campsites.rb
@@ -0,0 +1,28 @@
+
+#########################
+### SETUP/CONFIGURE ####
+########################
+
+#Require aws-sdk gem
+#Configure with Region, access key id and secret access key
+#Create new database
+
+
+
+########################
+### CREATE A TABLE ####
+#######################
+
+
+
+########################
+### ADD AN ITEM ####
+#######################
+
+
+
+#####################
+### RETRIEVE DATA ##
+####################
+
+#Query item by primary key
